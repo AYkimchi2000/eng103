f0e2f3629683fc5f686e404fbb5479af8262f10f
Arthur Yeh <136215568+AYkimchi2000@users.noreply.github.com>
2025-01-23 13:15:01 +0800

    Add files via upload
    assignmentr b

---
+++ factors.py
@@ -0,0 +1,6 @@
+target_int = int(input("Please enter a positive integer:"))
+print(f"The factors of {target_int} are:")
+for i in range(target_int):
+    p = i + 1
+    if target_int % p == 0:
+        print(p)
