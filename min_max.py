ef737c314f9c70b2f52a4a5d3421107f86eb812f
Arthur Yeh <136215568+AYkimchi2000@users.noreply.github.com>
2025-01-23 13:47:36 +0800

    Update min_max.py

--- min_max.py
+++ min_max.py
@@ -1,23 +1,16 @@
 print("How many integers would you like to enter?")
 loop_counter = int(input())
 print(f"Please enter {loop_counter} integers.")
-if loop_counter == 1:
-    user_input = int(input())
-    min = user_input
-    max = user_input
-else: 
-    min = 1
-    max = 1
-    while True:
-        if loop_counter == 0:
-            break
-        user_input = int(input())
-        loop_counter -= 1
-        
-        if user_input >= max:
-            max = user_input
-        if user_input <= min:
-            min = user_input
+user_input = int(input())
+min_value = user_input
+max_value = user_input
 
-print(f"min: {min}")
-print(f"max: {max}")
+for i in range(1, loop_counter): 
+    user_input = int(input())
+    if user_input < min_value:
+        min_value = user_input
+    if user_input > max_value:
+        max_value = user_input
+
+print(f"min: {min_value}")
+print(f"max: {max_value}")
