40ede11de2f9e66888ebae6aad2c0e0c344fb66a
Arthur Yeh <136215568+AYkimchi2000@users.noreply.github.com>
2025-01-30 13:05:56 +0800

    Add files via upload

---
+++ fall_distance.py
@@ -0,0 +1,7 @@
+def fall_distance(t):
+    """
+    function that calculates fall distance given time
+    t: time in seconds
+    return: fall distance in meter.
+    """
+    return 0.5 * 9.8 * t**2
