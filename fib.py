a1970f2284f8c873fcf469210cc85759ca433901
Arthur Yeh <136215568+AYkimchi2000@users.noreply.github.com>
2025-01-30 13:29:34 +0800

    Add files via upload

---
+++ fib.py
@@ -0,0 +1,17 @@
+def fib(x):
+    fibseq = [1, 1]
+    if x == 1:
+        return 1
+    if x == 2:
+        return 1
+    counter1 = 0
+    counter2 = 1
+    for i in range(x-2):
+        next_fib = fibseq[counter1] + fibseq[counter2]
+        counter1 += 1 
+        counter2 += 1
+        fibseq.append(next_fib)
+    output_fib = fibseq[x-3] + fibseq[x-2]
+    return output_fib
+
+
